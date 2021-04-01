21f3c8b20a5c91d18ff49e30f9cb8a9cbf946e95
Vasavi7 <vasavi7@outlook.com>
2021-03-31 22:32:42 -0500

    completed fetch and json studio.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+       console.log(response);
+    } );
+ });
